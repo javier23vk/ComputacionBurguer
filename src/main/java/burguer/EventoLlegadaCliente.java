57009a00492c0794484e2d40b4f150458c73ac0a
Javier Cabrera <cabrera.javier23@gmail.com>
2021-03-14 20:12:07 +0100

    fin finde 14/3

--- src/main/java/burguer/EventoLlegadaCliente.java
+++ src/main/java/burguer/EventoLlegadaCliente.java
@@ -1,12 +1,41 @@
 package burguer;
 
+import java.util.concurrent.TimeUnit;
+
+import co.paralleluniverse.fibers.SuspendExecution;
+import desmoj.core.simulator.Event;
+import desmoj.core.simulator.Model;
 import desmoj.core.simulator.TimeSpan;
 
-public class EventoLlegadaCliente {
+public class EventoLlegadaCliente  extends Event<Cliente>{
 
-	public void schedule(Cliente cliente, TimeSpan timeSpan) {
-		// TODO Auto-generated method stub
-		
+	private Modelo myModel;
+	
+	 public EventoLlegadaCliente(Model owner, String name, boolean showInTrace) {
+	      super(owner, name, showInTrace);
+	      // store a reference to the model this event is associated with
+	      myModel = (Modelo)owner;
+	   }
+	 
+	public void eventRoutine(Cliente cli) throws SuspendExecution {
+		myModel.colaClientes.insert(cli);
+	    sendTraceNote("ClientQueueLength: "+ myModel.colaClientes.length());
+	    if(!myModel.colaDependientes.isEmpty())
+	    {
+
+	         Dependiente dep = myModel.colaDependientes.first();
+
+	         myModel.colaDependientes.remove(dep);
+
+	         myModel.colaDependientes.remove(cli);
+
+	         // create a service end event
+	         ServiceEndEvent serviceEnd = new ServiceEndEvent (myModel,
+	                                                       "ServiceEndEvent", true);
+
+	         // and place it on the event list
+	         serviceEnd.schedule(dep, cli, new TimeSpan(myModel.getTiempoServicioDependientes(), TimeUnit.MINUTES));
+	    }
 	}
 
 }
---
+++ src/main/java/burguer/ServiceEndEvent.java
@@ -0,0 +1,12 @@
+package burguer;
+
+import desmoj.core.simulator.TimeSpan;
+
+public class ServiceEndEvent {
+
+	public void schedule(Dependiente dep, Cliente cli, TimeSpan timeSpan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
