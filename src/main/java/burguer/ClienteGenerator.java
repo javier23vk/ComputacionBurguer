b4641fa01b88818a019623f4c3d34aae8e0dc690
Adrian <cabrera23.adrian@gmail.com>
2021-03-14 19:37:37 +0100

    Cliente generator

---
+++ src/main/java/burguer/ClienteGenerator.java
@@ -0,0 +1,27 @@
+package burguer;
+
+import desmoj.core.simulator.ExternalEvent;
+
+import java.util.concurrent.TimeUnit;
+
+import desmoj.core.simulator.*;
+
+public class ClienteGenerator extends ExternalEvent {
+
+	public ClienteGenerator(Model owner, String name, boolean showInTrace) {
+		super( owner,  name,  showInTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void eventRoutine() {
+		// TODO Auto-generated method stub
+		Modelo model = (Modelo)getModel();
+		Cliente cli= new Cliente(model, "Cliente",true);
+		ClientArrivalEvent clientArrival= new ClientArrivalEvent(model,"ClientArrivalEvent",true);
+		clientArrival.schedule(cli,new TimeSpan(0, TimeUnit.MINUTES));
+	    schedule(new TimeSpan(model.getClientArrivalTime(), TimeUnit.MINUTES));
+
+		
+	}
+
+}
