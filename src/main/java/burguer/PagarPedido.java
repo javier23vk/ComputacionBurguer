d24417aaae7055367e5334886c5235c114811763
Adrian <cabrera23.adrian@gmail.com>
2021-03-21 14:18:34 +0100

    Cambios pagar pedido

---
+++ src/main/java/burguer/PagarPedido.java
@@ -0,0 +1,7 @@
+package burguer;
+
+import desmoj.core.simulator.EventOf2Entities;
+
+public class PagarPedido extends EventOf2Entities<E, F> {
+
+}
---
+++ src/main/java/burguer/PrepararHamburguesa.java
@@ -0,0 +1,13 @@
+package burguer;
+
+import desmoj.core.simulator.EventOf2Entities;
+import desmoj.core.simulator.Model;
+
+public class PrepararHamburguesa extends EventOf2Entities<E, F> {
+
+	public PrepararHamburguesa(Model arg0, String arg1, boolean arg2) {
+		super(arg0, arg1, arg2);
+		// TODO Auto-generated constructor stub
+	}
+
+}
