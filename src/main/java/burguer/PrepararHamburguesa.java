fe0b6fefb34d9c5b582e7900f345b970ff148989
Javier Cabrera <cabrera.javier23@gmail.com>
2021-03-27 18:48:57 +0100

    ytkdxk

--- src/main/java/burguer/PrepararHamburguesa.java
+++ src/main/java/burguer/PrepararHamburguesa.java
@@ -2,12 +2,13 @@ package burguer;
 
 import co.paralleluniverse.fibers.SuspendExecution;
 import desmoj.core.simulator.Event;
+import desmoj.core.simulator.EventOf2Entities;
 import desmoj.core.simulator.Model;
 import desmoj.core.simulator.TimeSpan;
 
 import java.util.concurrent.TimeUnit;
 
-public class PrepararHamburguesa extends Event<Cocinero> {
+public class PrepararHamburguesa extends EventOf2Entities<Cocinero, Dependiente> {
 	private Modelo myModel;
 
 	public PrepararHamburguesa(Model arg0, String arg1, boolean arg2) {
@@ -18,17 +19,36 @@ public class PrepararHamburguesa extends Event<Cocinero> {
 	}
 
 	@Override
-	public void eventRoutine(Cocinero coc) throws SuspendExecution {
-		// TODO Auto-generated method stub
-		if(!myModel.colaCocineros.isEmpty()){
-			Cocinero coci = myModel.colaCocineros.first();
-			myModel.colaCocineros.remove(coci);
-			myModel.colaCocineroComidaLista.insert(coci);
-			EntregaComida evento = new EntregaComida(myModel,"entrega comida",true);
-			evento.schedule(coci,new TimeSpan(myModel.getTiempoServicioCocineros(), TimeUnit.MINUTES));
+	public void eventRoutine(Cocinero arg0, Dependiente arg1) {
 
+		// Entregar al dependiente que estaba esperandome
+		// El dependiente cobra
+		EventoPago event = new EventoPago(myModel, "Evento Pago", true);
+		// lo programamos en la lista de eventos
+		event.schedule(dependiente, new TimeSpan(myModel.getTiempoPago()));
+		
+		
+		//Atender al siguiente
+		if (!myModel.colaPedidosPendientes.isEmpty()) {
+			Dependiente nextDependiente = myModel.colaPedidosPendientes.first();
+			// Y eliminar al dependiente de la cola de espera
+			myModel.colaPedidosPendientes.remove(nextDependiente);
+			
+			// creamos un nuevo evento para que cocine
+			EventoCocina nextEvent = new EventoCocina(myModel, "Evento Cocina", true);
+ 			// se programa el tiempo necesario para que termine de cocinar
+			nextEvent.schedule(cocinero, nextDependiente, new TimeSpan(myModel.getTiempoCocina(), TimeUnit.MINUTES));
+		}else {
+			// NO, no hay ningun pedido esperando
+
+			// el dependiente se incluye en la cola de dependientes ociosos
+			myModel.colaCocinerosOciosos.insert(cocinero);
+
+			// ahora esta esperando a que llegue algun dependiente
 		}
-
 	}
+
+
+	
 
 }
